645f1c670f5267b92999aa64032a9ba43f62e499
R John Robert <rjohn.robert@kalvium.community>
2025-01-06 00:26:30 +0530

    Milestone 9 done

--- Front_End/frontend/src/pages/HomePage.jsx
+++ Front_End/frontend/src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { useState } from 'react';
 import Card from '../component/ProductCard/Card';
 
@@ -10,19 +9,17 @@ function HomePage() {
   console.log(data);
 
   return (
-    <>
+    <div>
       <h1 className="text-center">Home Page for Follow Along</h1>
 
       <div className="grid grid-cols-3">
-        {data.map((ele, index) => {
-          return (
-            <div key={index} style={{ margin: 'auto' }}>
-              <Card title={ele.title} Index={index} />
-            </div>
-          );
-        })}
+        {data.map((ele, index) => (
+          <div key={index} style={{ margin: 'auto' }} className="border border-white">
+            <Card title={ele.title} Index={index} />
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
