ce2521354b4c1ab5a483a09ea78c128256c91254
R John Robert <rjohn.robert@kalvium.community>
2025-01-13 15:39:45 +0530

    Milestone 15 done

--- Front_End/frontend/src/App.jsx
+++ Front_End/frontend/src/App.jsx
@@ -4,17 +4,19 @@ import Signup from './pages/SignUp';
 import Login from './pages/Login';
 import ProductEntryPage from './pages/ProductEntryPage';
 import UpdateForm from './pages/UpdateForm';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/product-entry-page" element={<ProductEntryPage />} />
-        <Route path="/update-form/:id" element={<UpdateForm />} />
-      </Routes>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/product-entry-page" element={<ProductEntryPage />} />
+          <Route path="/update-form/:id" element={<UpdateForm />} />
+        </Routes>
     </>
   );
 }
