a2bfe58354cf163127674b148ba7b9e9de843cb7
R John Robert <rjohn.robert@kalvium.community>
2025-01-15 15:40:08 +0530

    Milestone 16 additional

--- Front_End/frontend/src/App.jsx
+++ Front_End/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
-import ProductEntryPage from './Pages/ProductEntryPage';
+import ProductEntryPage from './pages/ProductEntryPage';
 import UpdateForm from './Pages/UpdateForm';
 import Navbar from './components/Navbar/Navbar';
 import SinglePageProduct from './pages/SingleProductPage';
