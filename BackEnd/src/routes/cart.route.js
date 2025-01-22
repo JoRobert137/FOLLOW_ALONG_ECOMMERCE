a17dab1e18dc40cefd955213dabcc52f72f12064
R John Robert <rjohn.robert@kalvium.community>
2025-01-22 15:29:22 +0530

    M19 Done

--- BackEnd/src/routes/cart.route.js
+++ BackEnd/src/routes/cart.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const verifyUser = require('../middlwares/jwt-verify.js')
 const { AddToCartController, GetProductsForUser } = require('../controllers/cart.controller.js')
 
-router.post('add-to-cart', verifyUser, AddToCartController);
-router.get('get-user-cart-data', verifyUser, GetProductsForUser);
+router.post('/add-to-cart', verifyUser, AddToCartController);
+router.get('/get-user-cart-data', verifyUser, GetProductsForUser);
 
 module.exports = router;
