cbd884a0cdce6e4ca2382d502b0f4fa5d3ebc9b0
R John Robert <rjohn.robert@kalvium.community>
2025-02-07 15:40:00 +0530

    28th Milestone Completed

--- BackEnd/src/routes/order.route.js
+++ BackEnd/src/routes/order.route.js
@@ -5,9 +5,11 @@ const verifyUser  = require('../middlwares/jwt-verify');
 const {
     GetUserOrdersController,
     CreateOrderController,
+    CancelOrder,
   } = require('../controllers/order.controller.js');
 
 router.get('/user-orders-data', verifyUser, GetUserOrdersController);
 router.post('/confirm-order', verifyUser, CreateOrderController);
+router.patch('cancel-order', verifyUser, CancelOrder);
 
 module.exports = router;
